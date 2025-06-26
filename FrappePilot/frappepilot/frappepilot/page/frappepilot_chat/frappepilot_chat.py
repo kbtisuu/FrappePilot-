cb6a639da92940eabd41be47b07fac8a7b021dd9
kbtisuu <124124659+kbtisuu@users.noreply.github.com>
2025-06-26 14:21:05 +0300

    Delete FrappePilot directory

--- FrappePilot/frappepilot/frappepilot/page/frappepilot_chat/frappepilot_chat.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2025, kbtisuu and contributors
-# For license information, please see license.txt
-
-import frappe
-
-def get_context(context):
-	# This function is called when the page is loaded
-	context.no_cache = 1
-
